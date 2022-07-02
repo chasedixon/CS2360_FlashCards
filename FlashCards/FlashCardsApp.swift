bb5929a9e5b12bc5b731283345583de7119b4129
chasedixon <38643240+chasedixon@users.noreply.github.com>
2022-07-01 21:17:54 -0600

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Chase Dixon on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
